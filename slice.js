56537baa0edfdef53ba03db195e01d7a3d8cd80d
Habibur Rahman <habibur88454@gmail.com>
2024-09-02 22:12:45 +0600

    slice,join split apply

---
+++ slice.js
@@ -0,0 +1,12 @@
+const myLocation = "Dhanmondi";
+const somePart = myLocation.slice(0, 4);
+// console.log(somePart)
+
+const friendsName = 'jamal kamal rahim kajol sojol';
+console.log(friendsName)
+const friends = friendsName.split(' ');
+console.log(friends)
+
+const newFriends = [ 'jamal', 'kamal', 'rahim', 'kajol', 'sojol' ];
+const goodFriend = newFriends.join('|')
+console.log(goodFriend)
