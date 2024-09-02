bff314e40038955b861c0a26795aca0583028f25
Habibur Rahman <habibur88454@gmail.com>
2024-09-03 00:52:18 +0600

    object property and value added

---
+++ property.js
@@ -0,0 +1,21 @@
+const personInfos = {
+    name: 'Jamal Kodu',
+    age: 20,
+    isMarried: false,
+    car: 4,
+    'gf Names': ['alia', 'balia', 'dalia', 'kalia']
+}
+// console.log(personInfos)
+// dot notation ==== .
+// console.log(personInfos.age)
+const personCars = personInfos.car;
+// console.log(personCars)
+
+// console.log(personInfos['name'])
+const personName = personInfos['name'];
+// console.log(personName)
+
+// console.log(personInfos["gf Names"])
+
+const girlFrndName = personInfos["gf Names"];
+console.log(girlFrndName)
---
+++ value.js
@@ -0,0 +1,13 @@
+const personInfos = {
+    name: 'Jamal Kodu',
+    age: 20,
+    isMarried: false,
+    salary: 60000,
+    car: 4,
+    'gf Names': ['alia', 'balia', 'dalia', 'kalia']
+}
+
+const personName = 'Kolim uddin';
+personInfos.name = personName;
+personInfos.salary = 90000;
+console.log(personInfos)
