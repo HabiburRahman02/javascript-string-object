75397ddacacd56a6962b19d10262a4eb44d976c2
Habibur Rahman <habibur88454@gmail.com>
2024-09-02 22:22:03 +0600

    include and concat apply

---
+++ concat.js
@@ -0,0 +1,12 @@
+const firstName = 'Habibur';
+const lastName = 'Rahman';
+const fullName = firstName + " " + lastName
+// console.log(fullName)
+
+// concat a 1 er beshi value add korte caile koma akare bosano jabe
+const fullName2 = firstName.concat(' ',lastName);
+console.log(fullName2);
+
+
+console.log(lastName.includes('b'))
+console.log(lastName.includes('a'))
