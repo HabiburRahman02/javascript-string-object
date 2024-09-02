d3c0d9108ff9a9b42cf4b883346add85df19719f
Habibur Rahman <habibur88454@gmail.com>
2024-09-03 00:23:02 +0600

    object implement

---
+++ object.js
@@ -0,0 +1,22 @@
+const bottle = {
+    brand: 'aqua',
+    price: 340,
+    isImport: true,
+    color: 'green'
+}
+
+// console.log(bottle)
+
+const company = {
+    name: 'hb limited',
+    itsLocation: 'uttara',
+    employe: 45,
+    employeNames: ['jamal', 'kamal', 'mujib', 'josim'],
+    isMulti: true,
+    jodPositions: {
+        ceo: 'habib',
+        coo: 'Afjal',
+        marketingOfficer: 'josim mia'
+    }
+}
+console.log(company.jodPositions.ceo)
